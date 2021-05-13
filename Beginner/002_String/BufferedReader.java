35b63a6c2f8c7e431f0584b2be297554d77c621d
Saon00 <57843701+Saon00@users.noreply.github.com>
2021-05-13 21:46:19 +0600

    updated bufferedReader code

--- Beginner/002_String/BufferedReader.java
+++ Beginner/002_String/BufferedReader.java
@@ -2,20 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BufferedReader(
-  public static void main(String[] args) throws IOException {
+public class BufferedReader {
 
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		System.out.print("Name: ");
-		String name = bf.readLine();
-		System.out.print("Department: ");
-		String dept = bf.readLine();
-		System.out.print("Roll: ");
-		int roll = bf.read();
-		
-		System.out.printf("Your name is: %s\n"
-				+ "Your Department is: %s\n"
-				+ "Your Roll is: %d", name, dept, roll);
-	}
-)
+    public static void main(String[] args) throws IOException {
+
+        /*  You can use both of these way
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        */
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your Name: ");
+        String name = bufferedReader.readLine();
+        System.out.print("Enter your Age: ");
+        int age = Integer.parseInt(bufferedReader.readLine());
+        System.out.print("Enter your Country: ");
+        String countryName = bufferedReader.readLine();
+
+        System.out.println("Your Name is: " + name +
+                "\nYour Age is: " + age +
+                "\nYour Country is: " + countryName);
+
+
+    }
+}
+/*	Output:
+Your Name is: Srabon
+Your Age is: 22
+Your Country is: Bangladesh
+*/
+
+
+	
+	
